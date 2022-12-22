21ae0107a614b24f1b3cc263c6392a21aaa48ab6
SwordOfSouls <atolesnikov@gmail.com>
2022-12-21 20:06:06 -0800

    Skulls

--- src/main/java/org/soulsoftware/spigot/core/Utils/DataManager.java
+++ src/main/java/org/soulsoftware/spigot/core/Utils/DataManager.java
@@ -4,12 +4,18 @@
 
 package org.soulsoftware.spigot.core.Utils;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.MalformedJsonException;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,56 +23,68 @@ import org.bukkit.inventory.meta.SkullMeta;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class DataManager {
     @SneakyThrows
-    public static ItemStack setCustomSkullPlayer(ItemStack head, Player player) {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-        Class<?> strClass = Class.forName("org.bukkit.craftbukkit." + version + ".entity.CraftPlayer");
-        GameProfile profile = (GameProfile) strClass.cast(player).getClass().getMethod("getProfile")
-                .invoke(strClass.cast(player));
-        Property property = profile.getProperties().get("textures").iterator().next();
-        JSONParser parser = new JSONParser();
-        String id = ((String) ((JSONObject) ((JSONObject) ((JSONObject) parser.parse(new String(Base64.getDecoder().decode(property.getValue().getBytes())))).get("textures")).get("SKIN")).get(
-                "url"));
-        id = id.substring(38);
-        return setCustomSkullTexture(head, id);
+    public static ItemStack setCustomSkullPlayer(ItemStack head, OfflinePlayer player) {
+        URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + player.getUniqueId().toString().replace("-",""));
+        InputStreamReader read = new InputStreamReader(url.openStream());
+        JsonObject textureProperty = JsonParser.parseReader(read).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
+        read.close();
+        String texture = new String(Base64.getDecoder().decode(textureProperty.get("value").getAsString()));
+        texture = JsonParser.parseString(texture).getAsJsonObject().get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString().substring(38);
+        return setCustomSkullTexture(head, texture);
     }
 
     public static ItemStack setCustomSkullTexture(ItemStack head, String id) {
-        if (VersionManager.isAir(head)) {
-            if (hasNBT(head, "SkullOwner")) head = removeNBT(head, "SkullOwner");
-        }
-        id = id.replace(":", "");
-        String base64 = new String(Base64.getEncoder().encode(String.format("{\"textures\":{\"SKIN\":{\"url\":\"%s" +
-                        "\"}}}",
-                "http://textures.minecraft.net/texture/" + id).getBytes()));
-        Material material = Material.getMaterial("SKULL_ITEM");
-        if (material == null) {
-            head.setType(Material.getMaterial("PLAYER_HEAD"));
-        } else {
-            head.setType(material);
-            head.setDurability((short) SkullType.PLAYER.ordinal());
-            head.setData(material.getNewData((byte) SkullType.PLAYER.ordinal()));
-        }
-
-        GameProfile profile = new GameProfile(UUID.randomUUID(), "");
-        profile.getProperties().put("textures", new Property("textures", base64));
-        SkullMeta meta = (SkullMeta) head.getItemMeta();
         try {
-            Field profileField = meta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(meta, profile);
-        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException ex) {
-            ex.printStackTrace();
-        }
+            String base64;
+            try {
+                JsonReader reader = new JsonReader(new StringReader(new String(Base64.getDecoder().decode(id))));
+                JsonObject textureProperty = JsonParser.parseReader(reader).getAsJsonObject().get("textures").getAsJsonObject().get("SKIN").getAsJsonObject();
+                base64 = id;
+            } catch (IllegalStateException ex) {
+                if (VersionManager.isAir(head)) {
+                    if (hasNBT(head, "SkullOwner")) head = removeNBT(head, "SkullOwner");
+                }
+                id = id.replace(":", "");
+                base64 = new String(Base64.getEncoder().encode(String.format("{\"textures\":{\"SKIN\":{\"url\":\"%s" +
+                                "\"}}}",
+                        "http://textures.minecraft.net/texture/" + id).getBytes()));
+            }
+            Material material = Material.getMaterial("SKULL_ITEM");
+            if (material == null) {
+                head.setType(Material.getMaterial("PLAYER_HEAD"));
+            } else {
+                head.setType(material);
+                head.setDurability((short) SkullType.PLAYER.ordinal());
+                head.setData(material.getNewData((byte) SkullType.PLAYER.ordinal()));
+            }
 
-        head.setItemMeta(meta);
-        return head;
+            GameProfile profile = new GameProfile(UUID.randomUUID(), "");
+            profile.getProperties().put("textures", new Property("textures", base64));
+            SkullMeta meta = (SkullMeta) head.getItemMeta();
+            try {
+                Field profileField = meta.getClass().getDeclaredField("profile");
+                profileField.setAccessible(true);
+                profileField.set(meta, profile);
+            } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+
+            head.setItemMeta(meta);
+            return head;
+        } catch (Throwable io) {
+            io.printStackTrace();
+            return head;
+        }
     }
 
     public static ItemStack setNBT(ItemStack item, String key, String value) {
