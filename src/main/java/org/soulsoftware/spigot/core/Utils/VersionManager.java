009a2181b2b42c754c0aa9ddc6e3598da9344063
SwordOfSouls <atolesnikov@gmail.com>
2024-06-02 14:01:50 -0700

    Update to 1.20 Version Standard

--- src/main/java/org/soulsoftware/spigot/core/Utils/VersionManager.java
+++ src/main/java/org/soulsoftware/spigot/core/Utils/VersionManager.java
@@ -7,7 +7,17 @@ import org.bukkit.inventory.ItemStack;
 
 public class VersionManager {
     @Getter
-    private final static Integer serverVersion = Integer.parseInt(Bukkit.getServer().getClass().getName().split("\\.")[3].split("_")[1]);
+    private final static Double serverVersion;
+
+    static {
+        Double sV = 0d;
+        String[] scoreSplit = Bukkit.getServer().getClass().getName().split("\\.")[3].split("_");
+        sV += Integer.parseInt(scoreSplit[1]);
+        if(scoreSplit.length==3) sV += Integer.parseInt(scoreSplit[2].substring(1))/10d;
+
+        serverVersion = sV;
+
+    }
     public static boolean isAir(ItemStack stack) {
         if (stack == null) return true;
         return stack.getType().equals(Material.AIR);
